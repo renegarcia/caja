6f5acb52f9a3f182db2754c1c41561e3d66fac4c
Rene <garciamx@gmail.com>
2023-04-26 11:15:27 -0600

    User permissions and caja edit

--- urls.py
+++ urls.py
@@ -1,7 +1,9 @@
 from .views import (
     CajaListView,
     CajaCreateView,
+    CajaDeleteView,
     CajaDetailView,
+    CajaUpdateView,
     MovementCreateView,
     MovementCancelView,
 )
@@ -13,6 +15,8 @@ urlpatterns = [
     path("", CajaListView.as_view(), name="caja_list"),
     path("new", CajaCreateView.as_view(), name="caja_new"),
     path("<int:pk>/", CajaDetailView.as_view(), name="caja_detail"),
+    path("<int:pk>/edit", CajaUpdateView.as_view(), name="caja_update"),
+    path("<int:pk>/delete", CajaDeleteView.as_view(), name="caja_delete"),
     path("<int:pk>/movement/new", MovementCreateView.as_view(), name="movement_new"),
     path(
         "movement/<int:pk>/cancel", MovementCancelView.as_view(), name="movement_cancel"
--- views.py
+++ views.py
@@ -3,13 +3,21 @@ from .tables import MovementTable
 from .forms import MovementForm
 from .filters import MovementFilter
 from django_filters.views import FilterView
-from django.shortcuts import reverse
+from django.shortcuts import reverse, get_object_or_404
 from django.forms import modelform_factory
 from django.http import HttpResponseRedirect
-from django.views.generic import ListView, CreateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, CreateView, DeleteView, UpdateView
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
 from django.db import transaction
 from django_tables2 import SingleTableMixin
+
+
+class UserOwnsObjectMixin(UserPassesTestMixin):
+    def test_func(self):
+        obj = self.get_object()
+        return obj.user == self.request.user
 
 
 class CajaListView(LoginRequiredMixin, ListView):
@@ -37,10 +45,31 @@ class CajaCreateView(LoginRequiredMixin, CreateView):
         return HttpResponseRedirect(self.get_success_url())
 
 
-class CajaDetailView(LoginRequiredMixin, SingleTableMixin, FilterView):
+class CajaUpdateView(LoginRequiredMixin, UserOwnsObjectMixin, UpdateView):
+    model = Caja
+    fields = ("name",)
+    http_method_names = ("post",)
+
+
+class CajaDeleteView(LoginRequiredMixin, UserOwnsObjectMixin, DeleteView):
+    model = Caja
+    success_url = reverse_lazy("caja:caja_list")
+
+    def form_valid(self, form):
+        messages.success(self.request, "La caja fue eliminada")
+        return super(CajaDeleteView, self).form_valid(form)
+
+
+class CajaDetailView(
+    LoginRequiredMixin, UserPassesTestMixin, SingleTableMixin, FilterView
+):
     model = Movement
     table_class = MovementTable
     filterset_class = MovementFilter
+
+    def test_func(self):
+        caja = self.get_caja()
+        return caja.user == self.request.user
 
     def get_queryset(self):
         qs = super().get_queryset()
@@ -49,15 +78,29 @@ class CajaDetailView(LoginRequiredMixin, SingleTableMixin, FilterView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         pk = self.kwargs["pk"]
-        context["caja"] = Caja.objects.get(pk=self.kwargs["pk"])
+        caja = self.get_caja()
+        context["caja"] = caja
         context["form"] = MovementForm(initial={"caja": pk})
+        CajaForm = modelform_factory(Caja, fields=("name",))
+        context["caja_update_form"] = CajaForm(initial={"name": caja.name})
         return context
 
+    def get_caja(self):
+        try:
+            return self.caja
+        except AttributeError:
+            self.caja = get_object_or_404(Caja, pk=self.kwargs["pk"])
+        return self.caja
 
-class MovementCreateView(LoginRequiredMixin, CreateView):
+
+class MovementCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Movement
     fields = "__all__"
     http_method_names = ("post",)
+
+    def test_func(self):
+        caja = self.get_caja()
+        return caja.user == self.request.user
 
     def get_success_url(self, *args, **kwargs):
         return reverse("caja:caja_detail", kwargs={"pk": self.kwargs["pk"]})
@@ -75,10 +118,21 @@ class MovementCreateView(LoginRequiredMixin, CreateView):
         self.object = movement
         return HttpResponseRedirect(self.get_success_url())
 
+    def get_caja(self):
+        try:
+            return self.caja
+        except AttributeError:
+            self.caja = get_object_or_404(Caja, pk=self.kwargs["pk"])
+        return self.caja
 
-class MovementCancelView(LoginRequiredMixin, DeleteView):
+
+class MovementCancelView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Movement
     template_name = "caja/movement_confirm_cancel.html"
+
+    def test_func(self):
+        movement = self.get_object()
+        return movement.caja.user == self.request.user
 
     def get_success_url(self):
         if not self.success_url:
